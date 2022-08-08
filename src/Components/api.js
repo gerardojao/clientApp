cc42f135c1fa921826e33c671f58200f2ba53d8a
gerardojao <gerardojao@gmail.com>
2022-08-08 09:41:14 -0400

    remove / in url home and remove/ in api adding?

--- src/Components/api.js
+++ src/Components/api.js
@@ -4,7 +4,7 @@ import axios from "axios"
 export default axios.create({
   //baseURL: process.env.REACT_APP_SUPABASE_URL
    // baseURL: "https://localhost:44300/api"
-  baseURL: "http://projectfsmongo.somee.com/api"
+  baseURL: "http://projectfsmongo.somee.com?api"
    
 })
 // axios.defaults.baseURL = "https://oktara-back.herokuapp.com"
